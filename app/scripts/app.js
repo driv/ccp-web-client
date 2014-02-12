e8b9c39cf8b6c9e4b31d17bda840ff5db360f6af
driv <federiconafria@gmail.com>
2014-02-12 01:20:04 -0300

    Initial app generation and cleaning

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('ccpWebClientApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
